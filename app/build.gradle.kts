83208d309fe2a41c8f0e35716ee4ef6c3f9c13cc
sungbin5304 <sungbin.dev@gmail.com>
2020-09-08 00:41:35 +0900

    :partying_face: first commit! :partying_face:

---
+++ app/build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    id("com.android.application")
+    id("name.remal.check-dependency-updates") version "1.0.211"
+    id("com.google.gms.google-services")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    compileSdkVersion(Application.compileSdk)
+    defaultConfig {
+        minSdkVersion(Application.minSdk)
+        targetSdkVersion(Application.targetSdk)
+        versionCode = Application.versionCode
+        versionName = Application.versionName
+        multiDexEnabled = true
+        setProperty("archivesBaseName", "v$versionName ($versionCode)")
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+
+    packagingOptions {
+        exclude("META-INF/DEPENDENCIES")
+        exclude ("META-INF/library_release.kotlin_module")
+    }
+
+    compileOptions {
+        sourceCompatibility = Application.sourceCompat
+        targetCompatibility = Application.targetCompat
+    }
+
+    kotlinOptions {
+        jvmTarget = Application.jvmTarget
+    }
+}
+
+dependencies {
+    fun def(vararg dependencies: String) {
+        for (dependency in dependencies) implementation(dependency)
+    }
+
+    def(
+        Dependencies.Firebase.analytics,
+
+        Dependencies.Rx.Kotlin,
+        Dependencies.Rx.Android,
+        Dependencies.Rx.Retrofit,
+        Dependencies.Rx.Room,
+        Dependencies.Rx.Paging,
+
+        Dependencies.Essential.AppCompat,
+        Dependencies.Essential.Anko,
+        Dependencies.Essential.Kotlin,
+        Dependencies.Essential.LifeCycleExtensions,
+        Dependencies.Essential.LifeCycleViewModel,
+
+        Dependencies.Ktx.Paging,
+        Dependencies.Ktx.Room,
+        Dependencies.Ktx.Core,
+        Dependencies.Ktx.Fragment,
+
+        Dependencies.Di.Dagger,
+
+        Dependencies.Ui.YoYo,
+        Dependencies.Ui.Lottie,
+        Dependencies.Ui.Material,
+        Dependencies.Ui.Glide,
+        Dependencies.Ui.CardView,
+        Dependencies.Ui.ConstraintLayout,
+
+        Dependencies.Util.YoyoHelper,
+        Dependencies.Util.AndroidUtils,
+        Dependencies.Util.CrashReporter
+    )
+
+    kapt(Dependencies.Util.GlideCompiler)
+    kapt(Dependencies.Di.DaggerCompiler)
+    kapt(Dependencies.Jetpack.RoomCompiler)
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,120 @@
+import org.gradle.api.JavaVersion
+
+object Application {
+    const val minSdk = 23
+    const val targetSdk = 30
+    const val compileSdk = 30
+    const val jvmTarget = "1.8"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    val targetCompat = JavaVersion.VERSION_1_8
+    val sourceCompat = JavaVersion.VERSION_1_8
+}
+
+object Versions {
+    object Firebase {
+        const val analytics = "17.5.0"
+    }
+
+    object Rx {
+        const val Kotlin = "3.0.0"
+        const val Android = "3.0.0"
+        const val Retrofit = "2.9.0"
+    }
+
+    object Essential {
+        const val Google = "4.3.3"
+        const val AppCompat = "1.2.0"
+        const val Anko = "0.10.8"
+        const val Kotlin = "1.4.0"
+        const val Gradle = "4.0.1"
+        const val LifeCycleViewModel = "2.2.0"
+        const val LifeCycleExtensions = "2.2.0"
+    }
+
+    object Ktx {
+        const val Core = "1.3.1"
+        const val Fragment = "2.3.0"
+    }
+
+    object Di {
+        const val Dagger = "2.28.3"
+    }
+
+    object Jetpack {
+        const val Room = "2.3.0-alpha02"
+        const val Paging = "2.1.2"
+    }
+
+    object Ui {
+        const val YoYo = "2.4@aar"
+        const val Lottie = "3.4.2"
+        const val Material = "1.2.1"
+        const val Glide = "4.11.0"
+        const val CardView = "1.0.0"
+        const val ConstraintLayout = "2.0.1"
+    }
+
+    object Util {
+        const val YoYoHelper = "2.4@aar"
+        const val AndroidUtils = "3.2.6"
+        const val CarshReporter = "1.1.0"
+    }
+}
+
+object Dependencies {
+    object Firebase {
+        const val analytics = "com.google.firebase:firebase-analytics:${Versions.Firebase.analytics}"
+    }
+
+    object Rx {
+        const val Paging = "androidx.paging:paging-rxjava2:${Versions.Jetpack.Paging}"
+        const val Room = "androidx.room:room-rxjava2:${Versions.Jetpack.Room}"
+        const val Kotlin = "io.reactivex.rxjava3:rxkotlin:${Versions.Rx.Kotlin}"
+        const val Android = "io.reactivex.rxjava3:rxandroid:${Versions.Rx.Android}"
+        const val Retrofit = "com.squareup.retrofit2:adapter-rxjava3:${Versions.Rx.Retrofit}"
+    }
+
+    object Essential {
+        const val AppCompat = "androidx.appcompat:appcompat:${Versions.Essential.AppCompat}"
+        const val Anko = "org.jetbrains.anko:anko:${Versions.Essential.Anko}"
+        const val Kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.Essential.Kotlin}"
+        const val LifeCycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.Essential.LifeCycleExtensions}"
+        const val LifeCycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.Essential.LifeCycleViewModel}"
+    }
+
+    object Ktx {
+        const val Paging = "androidx.paging:paging-runtime-ktx:${Versions.Jetpack.Paging}"
+        const val Room = "androidx.room:room-compiler:${Versions.Jetpack.Room}"
+        const val Core = "androidx.core:core-ktx:${Versions.Ktx.Core}"
+        const val Fragment = "androidx.navigation:navigation-fragment-ktx:${Versions.Ktx.Fragment}"
+    }
+
+    object Di {
+        const val Dagger = "com.google.dagger:dagger:${Versions.Di.Dagger}"
+        const val DaggerCompiler = "com.google.dagger:dagger-compiler:${Versions.Di.Dagger}"
+    }
+
+    object Jetpack {
+        const val Room = "androidx.room:room-runtime:${Versions.Jetpack.Room}"
+        const val RoomCompiler = "androidx.room:room-compiler:${Versions.Jetpack.Room}"
+        const val Paging = "androidx.paging:paging-runtime:${Versions.Jetpack.Paging}"
+    }
+
+    object Ui {
+        const val YoYo = "com.daimajia.androidanimations:library:${Versions.Ui.YoYo}"
+        const val Lottie = "com.airbnb.android:lottie:${Versions.Ui.Lottie}"
+        const val Material = "com.google.android.material:material:${Versions.Ui.Material}"
+        const val Glide = "com.github.bumptech.glide:glide:${Versions.Ui.Glide}"
+        const val CardView = "androidx.cardview:cardview:${Versions.Ui.CardView}"
+        const val ConstraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.Ui.ConstraintLayout}"
+    }
+
+    object Util {
+        const val GlideCompiler = "com.github.bumptech.glide:compiler:${Versions.Ui.Glide}"
+        const val YoyoHelper = "com.daimajia.easing:library:${Versions.Util.YoYoHelper}"
+        const val AndroidUtils = "com.github.sungbin5304:SBT:${Versions.Util.AndroidUtils}"
+        const val CrashReporter = "com.balsikandar.android:crashreporter:${Versions.Util.CarshReporter}"
+    }
+}
