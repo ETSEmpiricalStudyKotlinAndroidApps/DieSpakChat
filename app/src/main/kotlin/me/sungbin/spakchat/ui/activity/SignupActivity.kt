370a43268692296854da5bed127326f34357a34d
sungbin5304 <sungbin.dev@gmail.com>
2020-09-11 11:49:24 +0900

    :unicorn:

--- app/src/main/kotlin/me/sungbin/spakchat/ui/activity/SignupActivity.kt
+++ app/src/main/kotlin/me/sungbin/spakchat/ui/activity/SignupActivity.kt
@@ -1,10 +1,16 @@
 package me.sungbin.spakchat.ui.activity
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_signup.*
 import me.sungbin.spakchat.R
+import me.sungbin.spakchat.ui.dialog.SigninBottomDialog
 import me.sungbin.spakchat.ui.dialog.SignupBottomDialog
 
 
@@ -24,10 +30,31 @@ class SignupActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
-
         btn_signup.setOnClickListener {
             val bottomSheetDialog = SignupBottomDialog.instance()
             bottomSheetDialog.show(supportFragmentManager, "")
+        }
+
+        btn_signin.setOnClickListener {
+            val bottomSheetDialog = SigninBottomDialog.instance()
+            bottomSheetDialog.show(supportFragmentManager, "")
+        }
+
+        tv_description.run {
+            val ssb = SpannableStringBuilder(text)
+            ssb.setSpan(
+                StyleSpan(Typeface.BOLD),
+                text.lastIndexOf("새로운"),
+                text.lastIndex+1,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            ssb.setSpan(
+                RelativeSizeSpan(1.6f),
+                text.lastIndexOf("새로운"),
+                text.lastIndex+1,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+            text = ssb
         }
     }
 
---
+++ app/src/main/kotlin/me/sungbin/spakchat/ui/dialog/SigninBottomDialog.kt
@@ -0,0 +1,37 @@
+package me.sungbin.spakchat.ui.dialog
+
+
+/**
+ * Created by SungBin on 2020-09-11.
+ */
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import me.sungbin.spakchat.R
+
+
+class SigninBottomDialog : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val layout = inflater.inflate(R.layout.layout_signin, container, false)
+        return layout
+    }
+
+    companion object {
+        private lateinit var bottomSheetDialogFragment: BottomSheetDialogFragment
+
+        fun instance(): BottomSheetDialogFragment {
+            if (!::bottomSheetDialogFragment.isInitialized) {
+                bottomSheetDialogFragment = SigninBottomDialog()
+            }
+            return bottomSheetDialogFragment
+        }
+    }
+}
