d6e538fedee6e7a0ddd0a55fd3adec7ff36b835e
sungbin5304 <sungbin.dev@gmail.com>
2020-09-25 13:23:36 +0900

    remove unused extensions

--- app/src/main/kotlin/me/sungbin/spakchat/util/extensions.kt
+++ app/src/main/kotlin/me/sungbin/spakchat/util/extensions.kt
@@ -1,17 +1,10 @@
 package me.sungbin.spakchat.util
 
 import android.widget.EditText
-import java.util.*
 
 
 /**
  * Created by SungBin on 2020-09-11.
  */
-
-fun Date.toCalendar(): Calendar {
-    val calendar = Calendar.getInstance()
-    calendar.time = this
-    return calendar
-}
 
 fun EditText.isBlank() = this.text.toString().isBlank()
